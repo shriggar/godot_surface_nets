c1077855dcac176ded0b3a2e31109cdecd074b0e
shriggar <shriggar@gmail.com>
2024-07-30 15:37:43 -0700

    uytfdi65do7fo7do7fyu

---
+++ junk_pile.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+@export var chunk_edge_length_in_voxels:int = 16
+@export var chunk_edge_length_in_meters:float = 16
+
+@export var chunk_offset:Vector3i = Vector3i.ZERO
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
